2e840a1701b159e51edfa43b5f71cd6c108b9f0a
matsufuji <n.matsufuji06@gmail.com>
2024-01-09 09:49:04 +0900

    モデルやコントローラを新規作成

---
+++ app/Models/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,4 @@
+<p>{{ $hello }}</p>
+@foreach ($hello_array as $hello_word)
+    {{ $hello_word }}<br>
+@endforeach
